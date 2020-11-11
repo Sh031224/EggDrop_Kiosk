459985fa74011c69fce6b45cadaff93dfc865a8b
Sh031224 <1cktmdgh2@gmail.com>
2020-11-11 12:10:08 +0900

    Add: SettingPreference

---
+++ EggDrop_Kiosk/EggDrop_Kiosk/Common/SettingPreference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EggDrop_Kiosk.Common
+{
+    class SettingPreference
+    {
+        //private 
+    }
+}
