240eb9a262fbc20920a558bfb782db491dfa83a6
Sh031224 <1cktmdgh2@gmail.com>
2020-11-11 12:09:11 +0900

    Add: Login

---
+++ EggDrop_Kiosk/EggDrop_Kiost.Core.Login/ViewModel/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using EggDrop_Kiost.Core.Login.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EggDrop_Kiost.Core.Login.ViewModel
+{
+    public class LoginViewModel
+    {
+        private LoginService loginService = new LoginService();
+
+        public Boolean TryLogin(String userId, String userPw)
+        {
+            return loginService.TryLogin(userId, userPw);
+        }
+    }
+}
