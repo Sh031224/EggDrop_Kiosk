e4f675b7f8f372ee8f8aa90854210724b17aa0c2
Sh031224 <1cktmdgh2@gmail.com>
2020-11-16 15:31:16 +0900

    Update AdminViewModel.cs

--- EggDrop_Kiosk/EggDrop_Kiosk.Core.Admin/ViewModel/AdminViewModel.cs
+++ EggDrop_Kiosk/EggDrop_Kiosk.Core.Admin/ViewModel/AdminViewModel.cs
@@ -1,19 +1,29 @@
-﻿using EggDrop_Kiosk.Core.Admin.Service;
+﻿using EggDrop_Kiosk.Core.Admin.Model;
+using EggDrop_Kiosk.Core.Admin.Service;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EggDrop_Kiosk.Core.Admin.ViewModel
 {
-    public class AdminViewModel
+    public class AdminViewModel: BindableBase
     {
         private OrderInfoService orderInfoService = new OrderInfoService();
 
+        private ObservableCollection<OrderInfoModel> _orderInfos = new ObservableCollection<OrderInfoModel>();
+        public ObservableCollection<OrderInfoModel> OrderInfoModel
+        {
+            get => _orderInfos;
+            set => SetProperty(ref _orderInfos, value);
+        }
+
         public void GetOrderInfo()
         {
-            orderInfoService.GetOrderInfoModels();
+            OrderInfoModel = orderInfoService.GetOrderInfoModels();
         }
     }
 }
